e006eef0e203b1a70ea62238f695124396d87c82
1Neikowo <j.silva23@ufromail.cl>
2023-08-24 10:02:51 -0400

    3er commit actividad 3,4

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        mostrarFilaMatriz(rellenarMatriz(crearMatriz(4,4)),1);
     }
 
 
@@ -16,6 +17,19 @@ public class Main {
     public static int[][] crearMatriz(int filas, int cols){
         int[][] matriz = new int[filas][cols];
         return matriz;
+    }
+    public static int[][] rellenarMatriz(int[][] matriz){
+        for (int i = 0; i < matriz.length ; i++) {
+            for (int j = 0; j <matriz[i].length ; j++) {
+                matriz[i][j] = (int) (Math.random()*9);
+            }
+        }
+        return matriz;
+    }
+    public static void mostrarFilaMatriz(int[][] matriz, int fila){
+        for (int i = 0; i < matriz[fila-1].length; i++) {
+            System.out.print("["+matriz[fila-1][i]+"]");
+        }
     }
 
 }
