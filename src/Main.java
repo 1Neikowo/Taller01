df8592e399496c71ff228b09c403409990adf5f4
1Neikowo <j.silva23@ufromail.cl>
2023-08-24 09:45:06 -0400

    2do commit actividad 1,2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
+
+
+    public static boolean validarDimensiones(int filas, int cols){
+
+        if (filas > 0 && cols>0){
+            return true;
+
+
+        }return false;
+    }
+    public static int[][] crearMatriz(int filas, int cols){
+        int[][] matriz = new int[filas][cols];
+        return matriz;
+    }
+
 }
